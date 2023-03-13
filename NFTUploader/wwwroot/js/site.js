b37a2ea672ab393c36ab8083eb4377bd88ea9cf4
Ivan Ishchenko <iishchenko@teambase.com>
2023-03-13 21:11:36 +0200

    added login with metamask

--- NFTUploader/wwwroot/js/site.js
+++ NFTUploader/wwwroot/js/site.js
@@ -1,4 +1,51 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.userWalletAddress = null;
 
-// Write your JavaScript code.
+const loginButton = document.getElementById('metamask_login');
+const userWallet = document.getElementById('userWallet');
+
+function toggleButton() {
+    if (!window.ethereum) {
+        loginButton.innerText = 'MetaMask is not installed';
+        loginButton.classList.remove('bg-purple-500', 'text-white');
+        loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed');
+        return false;
+    }
+
+    loginButton.addEventListener('click', loginWithMetaMask);
+}
+
+async function loginWithMetaMask() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        .catch((e) => {
+            console.error(e.message);
+            return;
+        });
+
+    if (!accounts) {
+        return;
+    }
+
+    window.userWalletAddress = accounts[0];
+    userWallet.innerText = window.userWalletAddress;
+    loginButton.innerText = 'Sign out of MetaMask';
+
+    loginButton.removeEventListener('click', loginWithMetaMask);
+    setTimeout(() => {
+        loginButton.addEventListener('click', signOutOfMetaMask);
+    }, 200);
+}
+
+function signOutOfMetaMask() {
+    window.userWalletAddress = null;
+    userWallet.innerText = '';
+    loginButton.innerText = 'Sign in with MetaMask';
+
+    loginButton.removeEventListener('click', signOutOfMetaMask);
+    setTimeout(() => {
+        loginButton.addEventListener('click', loginWithMetaMask)
+    }, 200);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    toggleButton();
+});
